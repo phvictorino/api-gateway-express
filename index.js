633287ff54d7ea67314a60f1bd3f3a5932c24739
Victorino <pedro.victorino@gmail.com>
2019-10-14 14:42:24 -0400

    auth passing header

--- index.js
+++ index.js
@@ -8,12 +8,18 @@ const axios = require('axios').default
 require('dotenv').config()
 
 const app = express()
+app.use(cookieParser())
 
 const authProxy = httpProxy(process.env.AUTH_URL)
-const credenciadoProxy = httpProxy(process.env.CREDENCIADO_URL)
+const credenciadoProxy = httpProxy(process.env.CREDENCIADO_URL, {
+  proxyReqOptDecorator: function(proxyReqOpts, srcReq) {
+    proxyReqOpts.headers = {user: srcReq.user};
+    return proxyReqOpts
+  }
+})
 
 const authMiddleware = async (req, res, next) => {
-  if (req.header('Authorization')) {
+  /* if (req.header('Authorization')) {
     await axios.post(process.env.AUTH_URL + '/decode', {
       token: req.header('Authorization').split(' ')[1]
     }).then(response => {
@@ -21,19 +27,23 @@ const authMiddleware = async (req, res, next) => {
     }).catch(error => {
       res.status(500).json({ message: error.message })
     })
-  }
+  } */
+  req.user = 1
   next()
 }
 
-app.use('/auth', authMiddleware, authProxy)
+app.use('/auth', authMiddleware, (req, res, next) => {
+  authProxy(req, res, next)
+})
 
-app.use('/credenciado', authMiddleware, credenciadoProxy)
+app.use('/credenciado', authMiddleware, (req, res, next) => {
+  credenciadoProxy(req, res, next)
+})
 
 app.use(logger('dev'))
 app.use(helmet())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
 
 const server = http.createServer(app)
 
